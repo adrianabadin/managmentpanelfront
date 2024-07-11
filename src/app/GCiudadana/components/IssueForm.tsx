28beee16f27b30e8f9354ac2722eaa03a5929fd5
Adrian Abadin <aabadin@gmail.com>
2024-07-11 19:18:34 -0300

    wtf

--- src/app/GCiudadana/components/IssueForm.tsx
+++ src/app/GCiudadana/components/IssueForm.tsx
@@ -119,7 +119,7 @@ export type UserIssue = z.infer<typeof userIssue>;
 export type FileType = z.infer<typeof FilesDescriptor>;
 function IssueForm() {
   const [open, setOpen] = useState(false);
-  const auth = useAppSelector((state) => state.auth);
+  //const auth = useAppSelector((state) => state.auth);
   const {
     register,
     setValue,
