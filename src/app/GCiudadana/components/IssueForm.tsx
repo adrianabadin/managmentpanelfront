16978bdab71e7db172aaffe58730c7fa161321c8
Adrian Abadin <aabadin@gmail.com>
2024-07-11 19:08:56 -0300

    wtf

--- src/app/GCiudadana/components/IssueForm.tsx
+++ src/app/GCiudadana/components/IssueForm.tsx
@@ -134,11 +134,11 @@ export function IssueForm() {
   const [createIssue] = useCreateIssueMutation();
   const { data, isFetching } = useGetStatesQuery(undefined);
   const { data: kois, isFetching: koisFetch } = useGetKOIsQuery(undefined);
-  if (
-    (Departments === undefined || Departments.length === 0) &&
-    isAdmin === false
-  )
-    return <div>Not Authorized</div>;
+  // if (
+  //   (Departments === undefined || Departments.length === 0) &&
+  //   isAdmin === false
+  // )
+  //   return <div>Not Authorized</div>;
   return isFetching || koisFetch ? (
     <Spinner />
   ) : (
